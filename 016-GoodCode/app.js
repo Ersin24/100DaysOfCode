a074bb72cde90a1be466d2e7c4e2f6b212f27ed7
Ersin Göç <ersin_1229@hotmail.com>
2022-12-13 21:06:46 +0300

    Good coding finished

--- 016-GoodCode/app.js
+++ 016-GoodCode/app.js
@@ -4,17 +4,16 @@ const express = require('express');
 const session = require('express-session');
 const csrf = require('csurf');
 
-const sessionConfig = require('./config/session')
+const sessionConfig = require('./config/session');
 const db = require('./data/database');
-const authRoutes = require('./routes/auth')
+const authRoutes = require('./routes/auth');
 const blogRoutes = require('./routes/blog');
 const authMiddleware = require('./middlewares/auth-middleware');
+const addCSRFTokenMiddleware = require('./middlewares/csrf-token-middleware');
 
 const mongoDbSessionStore = sessionConfig.createSessionStore(session);
 
 const app = express();
-
-
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -25,15 +24,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(session(sessionConfig.createSessionConfig(mongoDbSessionStore)));
 app.use(csrf());
 
+app.use(addCSRFTokenMiddleware);
 app.use(authMiddleware);
 
-//Rotalarda hangisinin üstte olduğunun önemi yok
-app.use(blogRoutes);
 app.use(authRoutes);
+app.use(blogRoutes);
 
-app.use(function(error, req, res, next) {
+app.use(function (error, req, res, next) {
   res.render('500');
-})
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
