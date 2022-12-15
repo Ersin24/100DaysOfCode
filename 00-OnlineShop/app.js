ee7f280f84d1ffa1e6dd451d6eb6461a83e4e202
Ersin Göç <ersin_1229@hotmail.com>
2022-12-15 13:11:41 +0300

    Adding Base Css files and static file serving

--- 00-OnlineShop/app.js
+++ 00-OnlineShop/app.js
@@ -9,6 +9,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+//Static files
+app.use(express.static('public'))
+
 app.use(authRoutes);
 
 app.listen(3000);
