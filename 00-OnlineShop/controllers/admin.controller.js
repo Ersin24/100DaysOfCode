db5dff2741567ad527b8d60dd6ad3c9cef9b7478
Ersin Göç <ersin_1229@hotmail.com>
2022-12-22 19:39:27 +0300

    More data: Adding a product Model and storing products ind the database

--- 00-OnlineShop/controllers/admin.controller.js
+++ 00-OnlineShop/controllers/admin.controller.js
@@ -1,22 +1,34 @@
+const Product = require("../models/product.model");
+
 function getProducts(req, res) {
-    res.render('admin/products/all-products')
+  res.render("admin/products/all-products");
 }
 
 function getNewProducts(req, res) {
-    res.render('admin/products/new-product')
+  res.render("admin/products/new-product");
 }
 
-function createNewProduct(req, res) {
-    console.log(req.body)
-    console.log(req.file)
+async function createNewProduct(req, res, next) {
+  // console.log(req.body)
+  // console.log(req.file)
 
-    res.redirect('/admin/products')
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename,
+  });
+
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  res.redirect("/admin/products");
 }
-
-
 
 module.exports = {
-    getProducts: getProducts,
-    getNewProducts: getNewProducts,
-    createNewProduct: createNewProduct,
-}
+  getProducts: getProducts,
+  getNewProducts: getNewProducts,
+  createNewProduct: createNewProduct,
+};
