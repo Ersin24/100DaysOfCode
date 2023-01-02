4f742d8664614d6954aa2e10bdd03c227ecfd966
Ersin Göç <ersin_1229@hotmail.com>
2023-01-02 13:40:01 +0300

    Updating Cart Items (Changing Quantities)

--- 00-OnlineShop/routes/cart.routes.js
+++ 00-OnlineShop/routes/cart.routes.js
@@ -8,4 +8,6 @@ router.get("/", cartController.getCart);
 
 router.post("/items", cartController.addCartItem); // /cart/items
 
+router.patch("/items", cartController.updateCartItem); //Güncelleme yaparken patch kullanıyoruz
+
 module.exports = router;
